291076d5afab095294e3012e274a5c7b07b299c3
DeokhunKim <koon0312@naver.com>
2022-09-25 22:32:31 +0900

    spring boot

---
+++ src/main/java/com/thekoon/kgc_information/KgcInformationApplication.java
@@ -0,0 +1,13 @@
+package com.thekoon.kgc_information;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KgcInformationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KgcInformationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thekoon/kgc_information/KgcInformationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thekoon.kgc_information;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KgcInformationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
